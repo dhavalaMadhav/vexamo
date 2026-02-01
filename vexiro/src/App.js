e406b37691e99f7734ee495b33d62ff138c84407
Bheesetty Charan Sai Prabha Kumar <charansaiprabhakumarbheesetty@gmail.com>
2026-02-01 19:03:48 +0530

    Import and include Work component in App

--- vexiro/src/App.js
+++ vexiro/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Services from './components/Services';
+import Work from './components/Work';
 import BackgroundSystem from './components/BackgroundSystem';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className="relative z-10">
         <Hero />
         <Services />
+        <Work />
       </div>
     </div>
   );
